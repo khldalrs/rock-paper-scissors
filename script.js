a532e2e1b0e2d2863464140b44bfe7edc2edc45a
khldalrs <khldalrs@gmail.com>
2023-05-20 21:07:02 +0400

    On branch UI  Changes to be committed: 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let playerScore = 0;
+let computerScore = 0;
+const winningScore = 5;
+
+document.getElementById("b1").addEventListener("click", function() {
+  playGame("rock");
+});
+document.getElementById("b2").addEventListener("click", function() {
+  playGame("paper");
+});
+document.getElementById("b3").addEventListener("click", function() {
+  playGame("scissors");
+});
+
 function computer() {
     let choices = ["rock", "paper", "scissors"];
     let computerSelection = choices[Math.floor(Math.random() * choices.length)];
@@ -6,7 +20,8 @@ function computer() {
 
 function playGame(playerSelection) {
     let computerSelection = computer();
-    var result = "";
+    let result = "";
+
     if (playerSelection === computerSelection) {
       result = "It's a tie!";
     } else if (
@@ -15,11 +30,25 @@ function playGame(playerSelection) {
       (playerSelection === "scissors" && computerSelection === "paper")
     ) {
       result = "You win!";
+      playerScore++;
     } else {
       result = "Computer wins!";
+      computerScore++;
     }
 
-    document.getElementById("result").innerHTML = result;
+    document.getElementById("result").textContent = result;
+    document.getElementById("score").textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
+
+    if (playerScore === winningScore) {
+      announceWinner("Player");
+    } else if (computerScore === winningScore) {
+      announceWinner("Computer");
+    }
 }
 
-  
+function announceWinner(winner) {
+    document.getElementById("result").textContent = `${winner} wins the game!`;
+    document.getElementById("b1").disabled = true;
+    document.getElementById("b2").disabled = true;
+    document.getElementById("b3").disabled = true;
+}
